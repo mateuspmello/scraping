6a19fc157c37035e76bc98e6510c9d93f2028660
Mateus Mello <mateus.mello@neoway.com.br>
2020-12-15 00:36:28 -0300

    secondo commit

--- secondo.py
+++ secondo.py
@@ -5,11 +5,10 @@ html = urlopen('http://pythonscraping.com/pages/warandpeace.html')
 bs = BeautifulSoup(html.read(), 'html.parser')
 nameList = bs.find_all(['span'],{'class':'green'})
 
-title = bs.find_all(class_='text', id="title")
+for name in nameList:
+    print(name.get_text())
 
-# for name in nameList:
-    # print(name.get_text())
+#busca dentro da tabela apenas os filhos e não os descendentes
+for child in bs.find('table', {'id':'giftList'}):
+    print(child)
 
-
-for t in title:
-    print(t)
